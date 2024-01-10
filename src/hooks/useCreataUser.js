a0a29bbbe18e21a118d8adc914788f7508393c76
Jamshidbek Codes <jamshidbekmirzakarimov6@gmail.com.>
2024-01-10 18:24:12 +0500

    fixed

--- src/hooks/useCreataUser.js
+++ src/hooks/useCreataUser.js
@@ -1,22 +1,28 @@
 import axios from "../api/api";
 import { toast } from "react-hot-toast";
-function useCreateUser() {
+
+function useCreateUser(token) {
   const create = async (values) => {
     const data = {
       fullName: values.text,
     };
     try {
-        const response = await axios.post("user/create", data);
-        console.log(response);
-      } catch (error) {
-        if (error.response) {
-          toast.error(error.response.data.message);
-        } else {
-          toast.error("An error occurred during login.");
-        }
+      const response = await axios.post("user/create", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      if (error.response) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("An error occurred during login.");
       }
+    }
   };
-  return {create}
+
+  return { create };
 }
 
 export default useCreateUser;
